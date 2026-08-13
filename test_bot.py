0a1df050c3984ea68f1ac0cf3a2bb91a923056aa
MatrixSparrow-coder <singhashok01801@gmail.com>
2026-08-13 10:38:26 +0530

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -1,5 +1,8 @@
+
+import asyncio
 from pyrogram import Client, filters
 from config import API_ID, API_HASH, BOT_TOKEN
+from web import run_web_server
 
 app = Client("test", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
@@ -8,4 +11,11 @@ async def start(client, message):
     print("GOT START!!!", flush=True)
     await message.reply_text("Test working!")
 
-app.run()
+async def main():
+    await app.start()
+    print("Test bot started.", flush=True)
+    await run_web_server()
+    await asyncio.Event().wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
